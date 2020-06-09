c986d65f05b4e78c61605d1d5ebda35eebd5e3c8
Riya Lohia <riyalh1997@gmail.com>
2020-06-09 23:12:52 +0530

    Updates

---
+++ precache-manifest.1eba73c838de1a06e43627e4b816bf55.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff9a1b0176dcfc6a4ced1e5b79b4f86c",
+    "url": "/custom-umbrella/index.html"
+  },
+  {
+    "revision": "0343997e87fa355faca7",
+    "url": "/custom-umbrella/static/css/main.306e9c62.chunk.css"
+  },
+  {
+    "revision": "1c18ed131512c45dbc0b",
+    "url": "/custom-umbrella/static/js/2.d13bf1c2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/custom-umbrella/static/js/2.d13bf1c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0343997e87fa355faca7",
+    "url": "/custom-umbrella/static/js/main.6639a8a0.chunk.js"
+  },
+  {
+    "revision": "79f8f8b0a7bdbdcb118d",
+    "url": "/custom-umbrella/static/js/runtime-main.ae5fca20.js"
+  },
+  {
+    "revision": "c0d6051cd2fe6bca587bf704e5b25b13",
+    "url": "/custom-umbrella/static/media/BlueUmbrella.c0d6051c.png"
+  },
+  {
+    "revision": "73894a0aab0335e5d8c35f0a8613858d",
+    "url": "/custom-umbrella/static/media/Loader.73894a0a.svg"
+  },
+  {
+    "revision": "d5e3c45b4935230ea079036da9aed1db",
+    "url": "/custom-umbrella/static/media/PinkUmbrella.d5e3c45b.png"
+  },
+  {
+    "revision": "bd7a6658753122243b1783b7857938ac",
+    "url": "/custom-umbrella/static/media/UploadIcon.bd7a6658.svg"
+  },
+  {
+    "revision": "ae59431f2b570f47f90c62ec1458b5b7",
+    "url": "/custom-umbrella/static/media/YellowUmbrella.ae59431f.png"
+  }
+]);
